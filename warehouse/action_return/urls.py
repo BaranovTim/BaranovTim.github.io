9e5c0b246070728ee4e24619cbaf7e9d438e4825
Sjx0ck <tim.dimytch@gmail.com>
2024-09-01 12:11:23 -0700

    Revert "Fixed qr scanner, added quantity page"
    This reverts commit a23e9b562dcf2ca5edca02ca7fa47f2b7a53d4c4.

--- warehouse/action_return/urls.py
+++ warehouse/action_return/urls.py
@@ -5,6 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.return_back, name='return_back'),
-    path('preaction_fifth/qrcodes/', include('qrcodes.urls')),
-    path('preaction_sixth/qrcodes/', include('qrcodes.urls')),
+    path('preaction_fifth/qrcodes', include('qrcodes.urls')),
+    path('preaction_sixth/qrcodes', include('qrcodes.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- warehouse/quantity/views.py
+++ warehouse/quantity/views.py
@@ -1,14 +1,4 @@
-from django.shortcuts import render, redirect
-from qrcodes.models import QRScan
+from django.shortcuts import render
 
 def quantity(request):
-    last_scan = QRScan.objects.last()
-
-    if request.method == 'POST':
-        quantity = int(request.POST.get('quantity'))
-        if 1 <= quantity <= 1000:
-            last_scan.quantity = quantity
-            last_scan.save()
-            return redirect('home')
-
-    return render(request, 'quantity/quantity.html', {'last_scan': last_scan})
+    return render(request,'quantity/quantity.html')
